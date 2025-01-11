82976525e4445852683b0dfbcd692cab12e8c8ae
prashant <prashanttt001@gmail.com>
2025-01-11 18:41:23 +0530

    removed node_modules

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,9 @@ function App() {
           <Route
             path="/"
             element={
-              <VideoSummarizer />
-
-              // <ProtectRoute>
-              // </ProtectRoute>
+              <ProtectRoute>
+                <VideoSummarizer />
+              </ProtectRoute>
             }
           />
           <Route path="/login" element={<Login />} />
